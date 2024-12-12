3d98a5dff60e17ff1305528f690fd06733e0530e
nilufer <niluse02@gmail.com>
2024-12-12 22:31:18 +0300

    feat: add gestures

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { LogBox } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </GestureHandlerRootView>
   );
 }
--- components/EmojiList.tsx
+++ components/EmojiList.tsx
@@ -15,6 +15,8 @@ export default function EmojiList({ onSelect, onCloseModal }: Props) {
     require("../assets/images/emoji4.png"),
     require("../assets/images/emoji5.png"),
     require("../assets/images/emoji6.png"),
+    require("../assets/images/emoji7.png"),
+    require("../assets/images/emoji8.png"),
   ]);
 
   return (
